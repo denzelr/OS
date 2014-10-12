702a062174b55b741f7f8f8650684ae304d8f47c
user <ryan.denzel@colorado.edu>
2014-10-12 15:49:02 -0600

    finished pseudocode

--- multi-lookup.c
+++ multi-lookup.c
@@ -10,16 +10,16 @@
 #define USAGE "<inputFilePath> <outputFilePath>"
 #define SBUFSIZE 1025
 #define INPUTFS "%1024s"
+#define THREADMAX 4
 
 pthread_mutex_t m;
 pthread_cond_t prod;
 pthread_cond_t con;
-int threadcount = 4;
-
-queue_init(queue *q, QUEUEMAXSIZE);
 
 int main(int argc, char* argv[]){
-	//error handeling
+	//argument error handeling
+	//queue_init(queue *q, QUEUEMAXSIZE); && check for queue failure
+	
 	//for(all files) 
 		//create producer thread
 
@@ -35,6 +35,15 @@ int main(int argc, char* argv[]){
 }
 
 void producer(){
+	//In each file, iterate through lines
+	//Create mutex lock
+	//wait if queue is full
+	//queue push address
+	//send signal
+	//unlock mutex
+	//close input file
+	
+
 	while(!finished){
 		pthread_mutex_lock(&m);
 		while(counter >= SBUFSIZE){
@@ -46,6 +55,16 @@ void producer(){
 }
 
 void consumer(){
+	//while were not finished or there is stuff in queue
+	//		take out mutex lock
+	//		condition handeling
+	//		pop a host name off queue
+	//      DNS lookup
+	//		print to output file
+	//		signal
+	//		unlock
+	//		exit thread??
+
 	while(!finished || counter > 0){
 		pthread_mutex_lock(&m);
 		while(count == 0){
